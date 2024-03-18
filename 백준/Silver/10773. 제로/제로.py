6b0c3038877102ad40392108248da2c1c349c6fe
ChoiDongjun <58469870+Choidongjun0830@users.noreply.github.com>
2024-03-18 17:33:43 +0900

    [Silver IV] Title: 제로, Time: 76 ms, Memory: 31900 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+K = int(input())
+result = []
+for i in range(K):
+    n = int(input())
+    if n != 0:
+        result.append(n)
+    else:
+        result.pop()
+print(sum(result))
