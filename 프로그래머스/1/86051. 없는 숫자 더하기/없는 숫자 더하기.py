42dd4a47989e2ea60fb98eeca6c5fda22e007df4
ChoiDongjun <58469870+Choidongjun0830@users.noreply.github.com>
2024-03-27 22:25:41 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -1,7 +1,6 @@
 def solution(numbers):
-    nums = [0,1,2,3,4,5,6,7,8,9]
-    for number in numbers:
-        if number in nums:
-            nums.remove(number)
-    answer = sum(nums)
+    answer = 0
+    for i in range(10):
+        if i not in numbers:
+            answer += i
     return answer
